72c011948690c829b5e009dbf4d30849be9c0c89
Mudkip <me@mudkip.pw>
2023-01-26 20:24:51 -0330

    re-enable countdown timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,10 +21,8 @@ WebFont.load({
 // Countdown Timer Script
 //
 
-var timer;
-
 // CURRENT DATE AND TIME
-var now = new Date();
+const now = new Date();
 
 /* OFFSET FOR Greenwich Mean Time from UTC:
  * For each offset, you add 1 hour (60 minutes)
@@ -33,35 +31,32 @@ var now = new Date();
  * EST -05:00 would be 0 - 60 - 60 - 60 - 60 = -240 (5 hours behind)
  * NST -03:30 would be 0 - 60 - 60 - 30 = -150 (3 hours and 30 minutes behind)
  */
-var offset = now.getTimezoneOffset() - 150;
+const offset = now.getTimezoneOffset() - 150;
 
 /* SET DATE TO SPECIFIC DAY IN THE FUTURE
  * MONTHS go from 0 to 11: January is 0, February is 1, and so on
  */
-var then = new Date(2023, 01, 17, 18, 00, 0, 0);
+const then = new Date(2023, 01, 17, 18, 00, 0, 0);
 
 // COUNT DOWN TO 3 DAYS IN THE FUTURE (259,200,000 = 1000 * 60 * 60 * 24 * 3)
 // var then = now.getTime() + 259200000;
-var compareDate = new Date(then) - now.getDate() - offset * 60 * 1000;
-timer = setInterval(function () {
+const compareDate = new Date(then) - now.getDate() - offset * 60 * 1000;
+timer = setInterval(() => {
 	timeBetweenDates(compareDate);
 }, 1000);
 
-function timeBetweenDates(toDate) {
-	var dateEntered = new Date(toDate);
-	var now = new Date();
-	var difference = dateEntered.getTime() - now.getTime();
+const timeBetweenDates = (toDate) => {
+	const dateEntered = new Date(toDate);
+	const now = new Date();
+	const difference = dateEntered.getTime() - now.getTime();
 
 	if (difference <= 0) {
-		$('#days').text('0');
-		$('#hours').text('0');
-		$('#minutes').text('0');
-		$('#seconds').text('0');
+		$('.countdown-row').hide();
 	} else {
-		var seconds = Math.floor(difference / 1000);
-		var minutes = Math.floor(seconds / 60);
-		var hours = Math.floor(minutes / 60);
-		var days = Math.floor(hours / 24);
+		let seconds = Math.floor(difference / 1000);
+		let minutes = Math.floor(seconds / 60);
+		let hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
 
 		hours %= 24;
 		minutes %= 60;
