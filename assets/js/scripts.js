43288160ff807e926322acc0ea732d1126c524d1
Madison Emshey <73123387+mbemshey@users.noreply.github.com>
2023-02-25 10:30:43 -0330

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -36,7 +36,7 @@ const offset = now.getTimezoneOffset() - 150;
 /* SET DATE TO SPECIFIC DAY IN THE FUTURE
  * MONTHS go from 0 to 11: January is 0, February is 1, and so on
  */
-const then = new Date(2023, 01, 17, 18, 00, 0, 0);
+const then = new Date(2023, 02, 31, 18, 00, 0, 0);
 
 // COUNT DOWN TO 3 DAYS IN THE FUTURE (259,200,000 = 1000 * 60 * 60 * 24 * 3)
 // var then = now.getTime() + 259200000;
